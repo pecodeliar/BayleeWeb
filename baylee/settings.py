c78ea9f6919b8e9e8636d56dc0aa2728b1066ed2
Justice <progretivity@gmail.com>
2023-10-14 13:09:17 -0400

    Changed debug setting

--- baylee/settings.py
+++ baylee/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['IS_DEBUG']
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
